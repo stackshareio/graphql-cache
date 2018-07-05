3f4f0ca18a596911954f0cd3672a528b78b98b7d
Michael Kelly <michaelkelly322@gmail.com>
2018-07-05 00:59:34 -0400

    Add spec coverage for Fetcher

---
+++ spec/graphql/cache/fetcher_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+module GraphQL
+  module Cache
+    RSpec.describe Fetcher do
+      describe 'graphql-ruby instrumentation API' do
+        it { should respond_to :instrument }
+      end
+
+      describe '#instrument' do
+        let(:type)  { Schema.types['Customer'] }
+        let(:field) do
+          double(
+            'graphql-ruby field',
+            resolve_proc: ->(obj, args, ctx) { nil },
+            redefine: nil
+          )
+        end
+
+        it 'should redefine the resolution proc' do
+          expect(field).to receive(:redefine)
+          subject.instrument(type, field)
+        end
+      end
+    end
+  end
+end
